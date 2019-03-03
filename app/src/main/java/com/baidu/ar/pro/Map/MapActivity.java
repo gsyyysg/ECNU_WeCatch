ff0c0859f8582c2bd3eb98071650ac67f98968ca
why <faceeyes@126.com>
2019-03-03 14:40:08 +0800

    修复了定位不准的bug

--- app/src/main/java/com/baidu/ar/pro/Map/MapActivity.java
+++ app/src/main/java/com/baidu/ar/pro/Map/MapActivity.java
@@ -324,7 +324,7 @@ public class MapActivity extends Activity {
         //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
         //可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-        locationOption.setCoorType("gcj02");
+        locationOption.setCoorType("bd09ll");
         //可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
         locationOption.setScanSpan(1000);
         //可选，设置是否需要地址信息，默认不需要
